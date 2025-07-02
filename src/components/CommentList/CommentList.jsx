77f7ee7688cf09f0854e27f7fadeedd1d126128b
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-07-02 04:17:51 -0300

    Corrige problemas para passar nos testes Cypress

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Comment } from "../Comment";
+import { Comment } from '../Comment';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
